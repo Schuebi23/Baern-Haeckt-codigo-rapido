0a842bbbf008d9dc702fc49b20e24c5b7ac89c5f
Luca Ammann <luca.ammann@students.bfh.ch>
2025-08-22 20:53:28 +0200

    service and ui setup

---
+++ co-shop-core/src/main/java/codigorapido/coshopcore/CoShopCoreApplication.java
@@ -0,0 +1,13 @@
+package codigorapido.coshopcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoShopCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoShopCoreApplication.class, args);
+	}
+
+}
---
+++ co-shop-core/src/test/java/codigorapido/coshopcore/CoShopCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package codigorapido.coshopcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoShopCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

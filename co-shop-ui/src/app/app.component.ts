e1a8c574fc5bbfa3b734556be1a7b31abc2d0535
Luca Ammann <luca.ammann@students.bfh.ch>
2025-08-24 00:19:50 +0200

    show displayname

--- co-shop-ui/src/app/app.component.ts
+++ co-shop-ui/src/app/app.component.ts
@@ -1,12 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {LocalStorageService} from './general/local-storage.service';
+import {Member} from './group/group';
+import {of} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
-    imports: [RouterModule],
-    selector: 'app-root',
-    template: `
-      <router-outlet></router-outlet>
+  imports: [RouterModule, AsyncPipe],
+  selector: 'app-root',
+  styles: `
+    header {
+      max-width: 400px;
+      margin: 0 auto .5rem auto;
+    }
+  `,
+  template: `
+    @let member = member$ | async;
+    @if (member) {
+      <header>Hallo {{ member.displayName }}</header>
+    }
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
+  private readonly localStorageService = inject(LocalStorageService);
+  private readonly httpClient = inject(HttpClient);
+  private readonly memberId: number | null = this.localStorageService.getItem('memberId');
+  readonly member$ = this.memberId ? this.httpClient.get<Member>('http://localhost:8080/members/' + this.memberId) : of(null);
 }

569488617248c0b1fc7a959f660dcc8233cd77b7
Luca Ammann <luca.ammann@students.bfh.ch>
2025-08-23 09:48:03 +0200

    wip: group page ui

--- co-shop-ui/src/app/app.routes.ts
+++ co-shop-ui/src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {GroupsComponent} from './groups/groups.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/groups', pathMatch: 'full' },
-  { path: 'groups', component: GroupsComponent },
+  {path: '', redirectTo: 'groups', pathMatch: "full"},
+  {path: 'groups', component: GroupsComponent},
 ];
--- co-shop-ui/src/app/groups/groups.component.ts
+++ co-shop-ui/src/app/groups/groups.component.ts
@@ -1,16 +1,34 @@
 import {Component, inject} from '@angular/core';
 import {GroupsStore} from './groups.store';
+import {GroupJoinModalComponent} from '../group-join-modal/group-join-modal.component';
 
 @Component({
   selector: 'app-groups',
   templateUrl: './groups.html',
   styleUrls: ['./groups.css'],
   providers: [GroupsStore],
+  imports: [
+    GroupJoinModalComponent
+  ]
 })
 export class GroupsComponent {
   readonly store = inject(GroupsStore);
+  showModal = false;
 
   constructor() {
     this.store.loadFromServer();
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  onConfirm(code: string) {
+    console.log('Code:', code);
+    this.showModal = false;
+  }
+
+  onCancel() {
+    this.showModal = false;
   }
 }
